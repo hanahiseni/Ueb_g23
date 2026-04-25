6e421631e496b9f9bdf8e36dd34530a68d6c67b4
VleraKelmeendi <vlera.kelmendi3@student.uni-pr.edu>
2026-04-25 23:11:23 +0200

    changes

--- contact.js
+++ contact.js
@@ -2,18 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("contactForm");
   const status = document.getElementById("formStatus");
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    if (form.checkValidity()) {
-      status.textContent = "Submitted request successfully";
-      status.style.color = "#22c55e";
-      form.reset();
-    } else {
-      status.textContent = "Invalid, check your information again";
-      status.style.color = "#ef4444";
-    }
+  form.addEventListener("submit", function () {
+    status.textContent = "Submitting your request...";
+    status.style.color = "#22c55e";
   });
 });
-
-
